c1ca1edab2097aa9942df957f6a23d342dd88c2a
Franz Lutzenberger <franz.lutzenberger@gmail.com>
2021-12-06 16:20:40 +0100

    add basic routes and cors for app

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+import PostCreate from "./PostCreate";
+
+export const App = () => {
+  return (
+    <div className='container'>
+      <h1>Create Post</h1>
+      <PostCreate />
+    </div>
+  );
+};
