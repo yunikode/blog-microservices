129ebf5faa05a673c58fcde16d944b088c884a7e
Franz Lutzenberger <franz.lutzenberger@gmail.com>
2021-12-07 12:50:38 +0100

    basic functionality done

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import PostCreate from "./PostCreate";
+import PostList from "./PostList";
 
 export const App = () => {
   return (
     <div className='container'>
       <h1>Create Post</h1>
       <PostCreate />
+      <hr/>
+      <h1>Posts</h1>
+      <PostList />
     </div>
   );
 };
