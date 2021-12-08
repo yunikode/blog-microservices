e45459ecbf98a0b557974233f2dfb026803a9c46
Franz Lutzenberger <franz.lutzenberger@gmail.com>
2021-12-08 13:48:00 +0100

    refactor to eventbus

--- client/src/PostList.js
+++ client/src/PostList.js
@@ -7,14 +7,14 @@ const PostList = () => {
   const [posts, setPosts] = useState({});
 
   const fetchPosts = async () => {
-    const res = await axios.get("http://localhost:4000/posts");
+    const res = await axios.get("http://localhost:4002/posts");
 
     setPosts(res.data);
   };
 
   useEffect(() => {fetchPosts()}, []);
 
-  const renderPosts = Object.values(posts).map((post) => (
+  const renderedPosts = Object.values(posts).map((post) => (
     <div
       className="card"
       style={{ width: "30%", marginBottom: "20px" }}
@@ -22,15 +22,15 @@ const PostList = () => {
     >
       <div className="card-body">
         <h3>{post.title}</h3>
+        <CommentList comments={post.comments} />
         <CommentCreate postId={post.id} />
-        <CommentList postId={post.id} />
       </div>
     </div>
   ));
 
   return (
     <div className="d-flex flex-row flex-wrap justify-content-between">
-      {renderPosts}
+      {renderedPosts}
     </div>
   );
 };
---
+++ query/index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+const posts = {};
+
+app.get("/posts", (req, res) => {
+  res.send(posts);
+});
+
+app.post("/events", (req, res) => {
+  const { type, data } = req.body;
+
+  switch (type) {
+    case "PostCreated": {
+      const { id, title } = data;
+      posts[id] = { id, title, comments: [] };
+      break;
+    }
+    case "CommentCreated": {
+      const { id, content, postId } = data;
+      const post = posts[postId];
+      post.comments.push({ id, content });
+      break;
+    }
+    default: {
+      console.log("Unknown Event");
+    }
+  }
+
+  res.send({});
+});
+
+app.listen(4002, () => {
+  console.log("Query running on 4002");
+});
